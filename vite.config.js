0d57178440f424d67281c955bf3d03762a23c393
muh-furqon <37769126+muh-furqon@users.noreply.github.com>
2025-11-06 19:41:18 +0700

    fixing something

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/personal-todos-app/', // Set the base path for GitHub Pages
+  base: './', // Set the base path for GitHub Pages
 })
